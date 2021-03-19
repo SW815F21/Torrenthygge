0b55e67e0aad555428c0b143abed0560dc6434d4
ScumbagDog <lbrack17@student.aau.dk>
2021-03-19 11:09:31 +0100

    Added progress feedback

--- hello.go
+++ hello.go
@@ -1,22 +1,62 @@
 package main
 
 import "log"
+import "fmt"
 import "os"
 import "github.com/anacrolix/torrent"
 import "strconv"
+import "time"
 
 func main() {
 	cf := torrent.NewDefaultClientConfig()
 	cf.ListenPort, _ = strconv.Atoi(os.Getenv("TORRENT_CLIENT_PORT"))
 	c, _ := torrent.NewClient(cf)
 	defer c.Close()
-	t, _ := c.AddTorrentFromFile("denmark-latest.osm.pbf.torrent")
+	t, _ := c.AddTorrentFromFile("map.torrent")
+	torrentBar(t, false)
 	//Kan også laves som c.AddMagnetLink("bedstemagnetlink");
 	<-t.GotInfo()
 	t.DownloadAll()
 	c.WaitAll()
 	log.Println("ladies and gentlemen, we got him")
-	for true {
-		c.WriteStatus(log.Writer())
-	}
+}
+
+func torrentBar(t *torrent.Torrent, pieceStates bool) {
+	go func() {
+		if t.Info() == nil {
+			fmt.Printf("Getting info for %q\n", t.Name())
+			<-t.GotInfo()
+		}
+		var lastLine string
+		for {
+			var completedPieces, partialPieces int
+			psrs := t.PieceStateRuns()
+			for _, r := range psrs {
+				if r.Complete {
+					completedPieces += r.Length
+				}
+				if r.Partial {
+					partialPieces += r.Length
+				}
+			}
+			line := fmt.Sprintf(
+				"Downloading %q: %d/%d, %d/%d pieces completed (%d partial)\n",
+				t.Name(),
+				uint64(t.BytesCompleted()),
+				uint64(t.Length()),
+				completedPieces,
+				t.NumPieces(),
+				partialPieces,
+			)
+
+			if line != lastLine {
+				lastLine = line
+				os.Stdout.WriteString(line)
+			}
+			if pieceStates {
+				fmt.Println(psrs)
+			}
+			time.Sleep(time.Second)
+		}
+	}()
 }
